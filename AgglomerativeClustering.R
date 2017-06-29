915edb4d6efa3e922633c6cf58f82757460b85fc
Ansih Singh Walia <loveeanish@gmail.com>
2017-06-29 21:11:01 +0530

    Plot for Manhattan distance Metric added

--- AgglomerativeClustering.R
+++ AgglomerativeClustering.R
@@ -8,7 +8,7 @@ theme_set(theme_classic())
 #Hierarichiral Clustering in a form of Dendrogram used in Agglomerative Clustering
 
 #dist() function used to compute distance matrix-default is 'euclidean' distance
-hc <- hclust(dist(USArrests), "ave") #Agglomerative CLustering using Euclidean Distance as
+hc <- hclust(dist(USArrests,method="manhattan"), "ave") #Agglomerative CLustering using Euclidean Distance as
 #a Similarity Metric to cluster(group) data 
 
 ggdendrogram(hc,rotate = T, size=2)
