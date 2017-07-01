62bb8857bb78fc8dee8ffb7c8a53084b0d72e648
Ansih Singh Walia <loveeanish@gmail.com>
2017-07-01 21:01:21 +0530

    Plot Image added

--- DivergingPlot.R
+++ DivergingPlot.R
@@ -13,8 +13,10 @@ ggplot(aes(x = car_name,y = mpg_norm ),data=mtcars) +
   geom_point(aes(col=mpg_type),stat="identity",size= 6 ) + 
   scale_color_manual(name="Mileage",
                      labels = c("Above Average", "Below Average"),
-                     values = c("above"="#00ba38", "below"="#f8766d")) + 
-  geom_text(aes(label=mpg_norm),color="white",size= 2) + 
-  labs(title="A diverging Plot",subtitle="Normalized Mileage") +
+                     values = c("above"="#00FA9A", "below"="#FFA07A")) + 
+  #geom_text() to add the Text on Points
+  geom_text(aes(label=mpg_norm),color="black",size= 2) + 
+  labs(title="A diverging Plot",subtitle="Normalized Mileage",
+       x="Car Name",y="Normalized Mileage") +
   ylim(-2.5, 2.5) +
   coord_flip()
