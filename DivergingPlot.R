01ca4de07a75bd3fc5f8009e28fe2c84a67cacf9
Ansih Singh Walia <loveeanish@gmail.com>
2017-07-01 20:50:54 +0530

    Plot code added

--- DivergingPlot.R
+++ DivergingPlot.R
@@ -4,3 +4,17 @@
 
 require(ggplot2)
 theme_set(theme_bw())
+
+
+
+#Plotting
+
+ggplot(aes(x = car_name,y = mpg_norm ),data=mtcars) + 
+  geom_point(aes(col=mpg_type),stat="identity",size= 6 ) + 
+  scale_color_manual(name="Mileage",
+                     labels = c("Above Average", "Below Average"),
+                     values = c("above"="#00ba38", "below"="#f8766d")) + 
+  geom_text(color="white",size= 2) + 
+  labs(title="A diverging Plot",subtitle="Normalized Mileage") +
+  ylim(-2.5, 2.5) +
+  coord_flip()
