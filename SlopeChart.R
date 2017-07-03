1813210615ea38502e24455409c54d8ab9a91796
Ansih Singh Walia <loveeanish@gmail.com>
2017-07-03 18:23:53 +0530

    Plot labels and some preprocssing for Slope Plot

--- SlopeChart.R
+++ SlopeChart.R
@@ -28,7 +28,12 @@ plot<-ggplot(gdpdata) + geom_segment(aes(x=1,xend=2,y='1952',yend='1957',col=cla
   labs(x="", y="Mean of GdpPerCap") +  # Axis labels
   xlim(.5, 2.5) + ylim(0,(1.1*(max(gdpdata$`1952`, gdpdata$`1957`))))
 
+#adding Texts
+plot<-plot + geom_text(label=left_label, y=gdpdata$`1952`, x=rep(1, NROW(gdpdata)), hjust=1.1, size=3.5)
+plot <- plot + geom_text(label=right_label, y=gdpdata$`1957`, x=rep(2, NROW(gdpdata)), hjust=-0.1, size=3.5)
 
   
+plot <- plot + geom_text(label="Time 1", x=1, y=1.1*(max(gdpdata$`1952`, gdpdata$`1957`)), hjust=1.2, size=6)  # title
+plot <- plot + geom_text(label="Time 2", x=2, y=1.1*(max(gdpdata$`1952`, gdpdata$`1957`)), hjust=-0.1, size=6)  # title
   
 
