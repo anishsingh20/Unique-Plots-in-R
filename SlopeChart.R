9447ad70b9199768826b506792d7da7628069924
Ansih Singh Walia <loveeanish@gmail.com>
2017-07-03 18:15:12 +0530

    GPD classes added

--- SlopeChart.R
+++ SlopeChart.R
@@ -11,3 +11,10 @@ theme_set(theme_classic())
 gdpdata<-read.csv("https://raw.githubusercontent.com/anishsingh20/datasets/master/gdppercap.csv")
 
 colnames(gdpdata)<-c("Contienent","1952","1957")
+
+left_label <- paste(gdpdata$Contienent, round(gdpdata$`1952`),sep=", ")
+right_label <- paste(gdpdata$Contienent, round(gdpdata$`1957`),sep=", ")
+
+#Genearing classes to which the Contienents belong i.e GDP change from year 1952 to 1957
+gdpdata$class <- ifelse((gdpdata$`1957` - gdpdata$`1952`) < 0, "red", "blue")
+
