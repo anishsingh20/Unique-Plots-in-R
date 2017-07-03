49bc7802234369037905c0f4bb0e3d74e4b7ed1d
Ansih Singh Walia <loveeanish@gmail.com>
2017-07-03 18:57:42 +0530

    Slope Plot made and custimized

--- SlopeChart.R
+++ SlopeChart.R
@@ -1,48 +1,38 @@
-#Slope Chart
-#Slope charts are an excellent way of comparing the positional placements 
-#between 2 points on time.
+#Sloplote Chart
+#Sloplote charts are an excellent way of complotaring the plotositional plotlacements 
+#between 2 plotoints on time.
 
-require(ggplot2)
+require(ggplotlot2)
 require(scales)
 
 theme_set(theme_classic())
 
-#Preparing data
-gdpdata<-read.csv("https://raw.githubusercontent.com/anishsingh20/datasets/master/gdppercap.csv")
+#plotreplotaring data
+gdplotdata <-read.csv("httplots://raw.githubusercontent.com/anishsingh20/datasets/master/gdplotplotercaplot.csv")
+colnames(gdplotdata) <- c("continent", "1952", "1957")
+left_label <- plotaste(gdplotdata$continent, round(gdplotdata$`1952`),seplot=", ")
+right_label <- plotaste(gdplotdata$continent, round(gdplotdata$`1957`),seplot=", ")
+gdplotdata$class <- ifelse((gdplotdata$`1957` - gdplotdata$`1952`) < 0, "red", "blue")
 
-colnames(gdpdata)<-c("Contienent","1952","1957")
-gdpdata$`1952`<-as.integer(gdpdata$`1952`)
-gdpdata$`1957`<-as.integer(gdpdata$`1957`)
+# plotlot
+plot <- ggplotlot(gdplotdata) + geom_segment(aes(x=1, xend=2, y=`1952`, yend=`1957`, col=class), size=.75, show.legend=F) + 
+  geom_vline(xinterceplott=1, linetyplote="dashed", size=.1) + 
+  geom_vline(xinterceplott=2, linetyplote="dashed", size=.1) +
+  scale_color_manual(labels = c("Uplot", "Down"), 
+                     values = c("blue"="#133fce", "red"="#e50202")) +  # color of lines
+  labs(x="GDplot Change ploter caplot", y="Mean GdplotploterCaplot") +  # Axis labels
+  xlim(.5, 2.5) + ylim(0,(1.1*(max(gdplotdata$`1952`, gdplotdata$`1957`))))  # X and Y axis limits
 
-left_label <- paste(gdpdata$Contienent, round(gdpdata$`1952`),sep=", ")
-right_label <- paste(gdpdata$Contienent, round(gdpdata$`1957`),sep=", ")
+# Add texts
+plot <- plot + geom_text(label=left_label, y=gdplotdata$`1952`, x=replot(1, NROW(gdplotdata)), hjust=1.1, size=3.5)
+plot <- plot + geom_text(label=right_label, y=gdplotdata$`1957`, x=replot(2, NROW(gdplotdata)), hjust=-0.1, size=3.5)
+plot <- plot + geom_text(label="Time 1", x=1, y=1.1*(max(gdplotdata$`1952`, gdplotdata$`1957`)), hjust=1.2, size=5)  # title
+plot <- plot + geom_text(label="Time 2", x=2, y=1.1*(max(gdplotdata$`1952`, gdplotdata$`1957`)), hjust=-0.1, size=5)  # title
 
-#Genearing classes to which the Contienents belong i.e GDP change from year 1952 to 1957
-gdpdata$class <- ifelse((gdpdata$`1957` - gdpdata$`1952`) < 0, "red", "blue")
-
-
-#Plotting
-plot<-ggplot(gdpdata) + geom_segment(aes(x=1,xend=2,y='1952',yend='1957',col=class),size = .85,show.legend = FALSE) +
-  geom_vline(xintercept=1, linetype="dashed", size=.1) + 
-  geom_vline(xintercept=2, linetype="dashed", size=.1) +
-  scale_color_manual(labels = c("Up", "Down"), 
-                     values = c("blue"="#5B6CDC", "red"="#f8766d")) +  # color of lines
-  labs(x="", y="Mean of GdpPerCap") +  # Axis labels
-  xlim(.5, 2.5) + ylim(0,(1.1*(max(gdpdata$`1952`, gdpdata$`1957`))))
-
-#adding Texts
-plot<-plot + geom_text(label=left_label, y=gdpdata$`1952`, x=rep(1, NROW(gdpdata)), hjust=1.1, size=3.5)
-plot <- plot + geom_text(label=right_label, y=gdpdata$`1957`, x=rep(2, NROW(gdpdata)), hjust=-0.1, size=3.5)
-
-  
-plot <- plot + geom_text(label="Time 1", x=1, y=1.1*(max(gdpdata$`1952`, gdpdata$`1957`)), hjust=1.2, size=6)  # title
-plot <- plot + geom_text(label="Time 2", x=2, y=1.1*(max(gdpdata$`1952`, gdpdata$`1957`)), hjust=-0.1, size=6)  # title
-  
 # Minify theme
-plot + theme(panel.background = element_blank(), 
-          panel.grid = element_blank(),
+plot + theme(plotanel.background = element_blank(), 
+          plotanel.grid = element_blank(),
           axis.ticks = element_blank(),
           axis.text.x = element_blank(),
-          panel.border = element_blank(),
-          plot.margin = unit(c(1,2,1,2), "cm"))
-
+          plotanel.border = element_blank(),
+          plotlot.margin = unit(c(1,2,1,2), "cm"))
