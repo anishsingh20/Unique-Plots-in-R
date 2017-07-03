be773926e2170cf90ddca574e162ce2572fa7034
Ansih Singh Walia <loveeanish@gmail.com>
2017-07-03 19:08:22 +0530

    few changes

--- SlopeChart.R
+++ SlopeChart.R
@@ -1,6 +1,6 @@
-#Slop Plots Chart
-#Slop Plots charts are an excellent way of complotaring the plot positional plotlacements 
-#between 2 plotoints on time.
+#Slope Chart
+#Slope charts are an excellent way of complotaring the plot positional placements 
+#between 2 points on time.
 
 require(ggplotlot2)
 require(scales)
@@ -10,17 +10,17 @@ theme_set(theme_classic())
 #plotreplotaring data
 gdpdata <-read.csv("https://raw.githubusercontent.com/anishsingh20/datasets/master/gdppercap.csv")
 colnames(gdpdata) <- c("continent", "1952", "1957")
-left_label <- plotaste(gdpdata$continent, round(gdpdata$`1952`),seplot=", ")
-right_label <- plotaste(gdpdata$continent, round(gdpdata$`1957`),seplot=", ")
+left_label <- paste(gdpdata$continent, round(gdpdata$`1952`),sep=", ")
+right_label <- paste(gdpdata$continent, round(gdpdata$`1957`),sep=", ")
 gdpdata$class <- ifelse((gdpdata$`1957` - gdpdata$`1952`) < 0, "red", "blue")
 
-# plotlot
+# plot
 plot <- ggplot(gdpdata) + geom_segment(aes(x=1, xend=2, y=`1952`, yend=`1957`, col=class), size=.75, show.legend=F) + 
   geom_vline(xintercept=1, linetype="dashed", size=.1) + 
   geom_vline(xintercept=2, linetype="dashed", size=.1) +
-  scale_color_manual(labels = c("Uplot", "Down"), 
+  scale_color_manual(labels = c("Up", "Down"), 
                      values = c("blue"="#133fce", "red"="#e50202")) +  # color of lines
-  labs(x="GDplot Change per cap", y="Mean GdpPerCap") +  # Axis labels
+  labs(x="GDP Change per cap", y="Mean GdpPerCap") +  # Axis labels
   xlim(.5, 2.5) + ylim(0,(1.1*(max(gdpdata$`1952`, gdpdata$`1957`))))  # X and Y axis limits
 
 # Add texts
