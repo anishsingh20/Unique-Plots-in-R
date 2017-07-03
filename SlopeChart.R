c3c1f59f2f2d25f80cfac1752976854b19aa1d7a
Ansih Singh Walia <loveeanish@gmail.com>
2017-07-03 18:07:33 +0530

    Building Slope chart to compare positional changes

---
+++ SlopeChart.R
@@ -0,0 +1,8 @@
+#Slope Chart
+#Slope charts are an excellent way of comparing the positional placements 
+#between 2 points on time.
+
+require(ggplot2)
+require(scales)
+
+theme_set(theme_classic())
