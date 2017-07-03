5703a8f97e1bf16b8ab1121498e4c7e153e8599f
Ansih Singh Walia <loveeanish@gmail.com>
2017-07-03 18:20:38 +0530

    Base plot created

--- SlopeChart.R
+++ SlopeChart.R
@@ -18,3 +18,17 @@ right_label <- paste(gdpdata$Contienent, round(gdpdata$`1957`),sep=", ")
 #Genearing classes to which the Contienents belong i.e GDP change from year 1952 to 1957
 gdpdata$class <- ifelse((gdpdata$`1957` - gdpdata$`1952`) < 0, "red", "blue")
 
+
+#Plotting
+plot<-ggplot(gdpdata) + geom_segment(aes(x=1,xend=2,y='1952',yend='1957',col=class),size = .85,show.legend = FALSE) +
+  geom_vline(xintercept=1, linetype="dashed", size=.1) + 
+  geom_vline(xintercept=2, linetype="dashed", size=.1) +
+  scale_color_manual(labels = c("Up", "Down"), 
+                     values = c("blue"="#5B6CDC", "red"="#f8766d")) +  # color of lines
+  labs(x="", y="Mean of GdpPerCap") +  # Axis labels
+  xlim(.5, 2.5) + ylim(0,(1.1*(max(gdpdata$`1952`, gdpdata$`1957`))))
+
+
+  
+  
+
