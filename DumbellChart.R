93f5d5e6967dd881b29986ef5724d021261c45f3
Ansih Singh Walia <loveeanish@gmail.com>
2017-07-05 19:33:58 +0530

    Dumbell Plot

---
+++ DumbellChart.R
@@ -0,0 +1,9 @@
+#Dumbell Chart
+#Dumbell plots are also a great Tool to vislaualize the Relative Positions between the 2 points
+#like Growth and Decline
+
+require(ggplot2)
+devtools::install_github("hrbrmstr/ggalt")
+require(ggaalt)
+
+
