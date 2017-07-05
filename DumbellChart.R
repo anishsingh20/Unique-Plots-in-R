6f81e4cd84d234ded91ca4d908bd0ba7d3fe6f43
Ansih Singh Walia <loveeanish@gmail.com>
2017-07-05 20:01:09 +0530

    Made the Plot

--- DumbellChart.R
+++ DumbellChart.R
@@ -9,3 +9,26 @@ theme_set(theme_classic())
 
 #Loading the Dataset
 health<-read.csv("https://raw.githubusercontent.com/anishsingh20/datasets/master/health.csv")
+health$Area <- factor(health$Area, levels=as.character(health$Area))  
+# ordering of the dumbells
+
+
+#Making the Plot
+
+plot<-ggplot(aes(x=pct_2013,xend=pct_2014,y=Area , gropu = Area),data = health) +
+  geom_dumbbell(color="#a3c4dc",
+                size=1,
+                print.color.1="#0e668b") + 
+  labs(x="Percentage", 
+       y="City", 
+       title="Dumbell Plot for Positional Changes between 2 points", 
+       subtitle="Percent Change: 2013 vs 2014") + 
+        theme(plot.title = element_text(hjust=0.5, face="bold"),
+        plot.background=element_rect(fill="#f7f7f7"),
+        panel.background=element_rect(fill="#f7f7f7"),
+        panel.grid.minor=element_blank(),
+        panel.grid.major.y=element_blank(),
+        panel.grid.major.x=element_line(),
+        axis.ticks=element_blank(),
+        legend.position="top",
+        panel.border=element_blank())
