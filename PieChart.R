5a0f8c2345c3ce5cfdac227a8cd8b1ce8995ae63
Ansih Singh Walia <loveeanish@gmail.com>
2017-07-13 20:58:24 +0530

    A barplot made

--- PieChart.R
+++ PieChart.R
@@ -1,7 +1,7 @@
 #Piechart
 
 library(ggplot2)
-theme_set(theme_dark())
+theme_set(theme_classic())
 
 #Making the Pie Chart on mpg dataset
 
@@ -12,3 +12,12 @@ Carclass<-as.data.frame(table(mpg$class))
 #giving new names to the columns
 names(Carclass)<-c("class","count")
 
+#Making the Plot
+ 
+plot<-ggplot(aes(x = "" , y = count,fill=factor(class)),data = Carclass) + 
+  geom_bar(width=1 , stat="identity") + 
+  theme(axis.line = element_blank(), 
+        plot.title = element_text(hjust=0.5)) + 
+        labs(title="Pie Chart of distributions of the Class of Cars",
+             x=NULL,y=NULL , fill = 'Class of cars')
+  
