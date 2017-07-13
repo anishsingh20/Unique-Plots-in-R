5067405aaac839e5e334d0d36d3bf657d86aa55d
Ansih Singh Walia <loveeanish@gmail.com>
2017-07-13 20:53:30 +0530

    Did some data prepocessing to feed it to the ggplot

--- PieChart.R
+++ PieChart.R
@@ -1,5 +1,14 @@
 #Piechart
 
 library(ggplot2)
-theme_set(theme_classic())
+theme_set(theme_dark())
+
+#Making the Pie Chart on mpg dataset
+
+#making a Data frame consisting of Cartypes and its frequency
+
+Carclass<-as.data.frame(table(mpg$class))
+
+#giving new names to the columns
+names(Carclass)<-c("class","count")
 
